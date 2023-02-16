a43b4e2f6bd3d2fd2d03159a77393be96be765ec
mirzavic4869 <avicenamirzam@gmail.com>
2023-02-16 17:09:04 +0700

    struktur kendali

---
+++ StrukturKendali.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$i=0;
+for($i;$i<5; $i++ ) {
+echo "Hai Dunia ";
+}
+
+?>
---
+++ TipeDataNumber.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Floating point dengan E notation: ";
+var_dump(1.2e5);
